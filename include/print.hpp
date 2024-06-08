09d63440d39f4b2b22af9ec064591e69fbdee4cc
Sigma Park <greengb@naver.com>
2024-06-08 23:10:46 +0900

    feat: print library as DLL

--- include/print.hpp
+++ include/print.hpp
@@ -1,10 +1,20 @@
 #pragma once
 
+#ifdef BUILD_PRAC
+#define PRAC_API __declspec(dllexport)
+#else
+#define PRAC_API __declspec(dllimport)
+#endif
+
+
 namespace prac
 {
+	extern "C"
+	{
 
-	void Print(char const* const msg);
-	void Press_enter_to_exit();
+		void PRAC_API Print(char const* const msg);
+		void PRAC_API Press_enter_to_exit();
 
+	}
 }
 
